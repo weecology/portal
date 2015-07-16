18d8da945e8342fd86e8c74545e4186217e2d09d
Morgan Ernest <morgan@weecology.org>
2015-07-16 11:30:48 -0400

    Continuing to use this file to process dataset for timeseries analyses. Here I added filling NDVI NAs with means from flanking data and changed how date were being handled to be Year-Month in the Monthly NDVI file.

--- RemoteSensing/LS_time.R
+++ RemoteSensing/LS_time.R
@@ -1,24 +1,45 @@
 library(plyr)
 
+
 ### FUNCTION
 
-add_month = function(df){
+convert_julianday_to_monthyr = function(Year, JulianDay){
+
   
-  # takes the Julian Day from the dataframe and creates a column in the
-  # dataframe with the month of the NDVI value
-  
-  full_date <- paste(df$Year, df$JulDay, sep="-")
-  date = as.Date(strptime(full_date, format="%Y-%j"), format="%m-%d-%Y")
-  as.numeric(format(date, format = "%m")) 
+  combined <- paste(Year, JulianDay, sep="-")
+  full_date = as.Date(strptime(combined, format="%Y-%j"), format="%m-d-%Y")
+  date = as.Date(full_date, format="%Y-%j")
+  strftime(date, "%Y-%m")
 }
+
 
 ### MAIN CODE
 
 data = read.csv(file = "Landsat_NDVI_adj_allsats.csv")
 
 # Add time identifiers
-data$month = add_month(data)
+data$date = convert_julianday_to_monthyr(data$Year, data$JulDay)
 
-write.csv(month_scale, "monthly_NDVI.csv")
-# monthly_NDVI: median NDVI from all images from that month. N is the number
-# of images used to calculate the median. 
+month_w_NAs = ddply(data, ~ date, summarise,
+                    N = sum(!is.na(NDVI)),
+                    median = median(NDVI, na.rm=TRUE),
+                    sd = sd(NDVI, na.rm=TRUE))
+
+month_wo_NAs = month_w_NAs[order(month_w_NAs$date),] 
+
+NA_rows = which(is.na(month_wo_NAs$median))
+previous_row = NA_rows-1
+next_row = NA_rows+1
+
+NA_row_info = data.frame(NA_rows, previous_row, next_row)
+
+NA_row_info$previous_value = month_wo_NAs[NA_row_info[,2], c("median")]
+NA_row_info$next_value = month_wo_NAs[NA_row_info[,3], c("median")]
+NA_row_info$mean = (NA_row_info$previous_value + NA_row_info$next_value) /2
+
+NA_data = data.frame(NA_row_info$NA_rows, NA_row_info$mean)
+i=1
+for (row in NA_row_info$NA_rows){
+  month_wo_NAs[row,]$median = NA_row_info[i,]$mean
+  i= i+1
+}
