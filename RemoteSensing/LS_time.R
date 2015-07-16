c5639b8aa12383dd6b885dbb8d4919b59131fc31
Morgan Ernest <morgan@weecology.org>
2015-07-16 13:08:28 -0400

    Turns out there were more missing months than I thought because some months never had any images (as opposed to miages that couldn't be used), which means they were compeltely missing from the files. I have added code that finds the missing month and inserts that date and NA for the NDVI value. I have removed all the code that fills in NAs from surrounding data because the gaps are now bigger and figuring out how to best fill those will require more thought

--- RemoteSensing/LS_time.R
+++ RemoteSensing/LS_time.R
@@ -30,28 +30,16 @@ data = read.csv(file = "Landsat_NDVI_adj_allsats.csv")
 
 data$date = convert_julianday_to_monthyr(data$Year, data$JulDay)
 
-month_w_NAs = ddply(data, ~ date, summarise,
+month_missingdates = ddply(data, ~ date, summarise,
                     N = sum(!is.na(NDVI)),
                     median = median(NDVI, na.rm=TRUE),
                     sd = sd(NDVI, na.rm=TRUE))
 
-month_wo_NAs = month_w_NAs[order(month_w_NAs$date),] 
+month_missingdates$current_dates = as.Date(paste(month_missingdates$date,"-",15,sep=""))
+first_date = as.Date(min(month_missingdates$current_date))
+end_date = as.Date(max(month_missingdates$current_date))
 
-NA_rows = which(is.na(month_wo_NAs$median))
-previous_row = NA_rows-1
-next_row = NA_rows+1
+full = seq(from=first_date, to=end_date, by='1 month') 
 
-NA_row_info = data.frame(NA_rows, previous_row, next_row)
-
-NA_row_info$previous_value = month_wo_NAs[NA_row_info[,2], c("median")]
-NA_row_info$next_value = month_wo_NAs[NA_row_info[,3], c("median")]
-NA_row_info$mean = (NA_row_info$previous_value + NA_row_info$next_value) /2
-
-NA_data = data.frame(NA_row_info$NA_rows, NA_row_info$mean)
-i=1
-for (row in NA_row_info$NA_rows){
-  month_wo_NAs[row,]$median = NA_row_info[i,]$mean
-  i= i+1
-}
-
-write.csv(month_wo_NAs, "Monthly_NDVI.csv")
+all_months = data.frame(Date=full, NDVI = month_missingdates$median[match(full,month_missingdates$current_dates)])
+write.csv(all_months, "Monthly_NDVI.csv")
